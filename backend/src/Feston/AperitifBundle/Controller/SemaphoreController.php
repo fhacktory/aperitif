72bac7a90661dcb5dc20094ee62a934fe69a1324
Raphaël Lustin <raphael@lustin.fr>
2014-10-18 14:49:20 +0200

    Adds list and update actions for semaphore.

--- backend/src/Feston/AperitifBundle/Controller/SemaphoreController.php
+++ backend/src/Feston/AperitifBundle/Controller/SemaphoreController.php
@@ -10,6 +10,26 @@ use Feston\AperitifBundle\Entity\Semaphore;
 
 class SemaphoreController extends FOSRestController
 {
+    public function listAction()
+    {
+        $em = $this->getDoctrine()->getManager();
+        $semaphores = $em->getRepository('FestonAperitifBundle:Semaphore')->findAll();
+
+        $data = array();
+        foreach ($semaphores as $semaphore) {
+            $data[] = array(
+                'created' => $semaphore->getCreated(),
+                'publicId' => $semaphore->getPublicId(),
+                'username' => $semaphore->getUsername(),
+                'location' => $semaphore->getLocation(),
+            );
+        }
+
+        $view = $this->view($data, 200);
+
+        return $this->handleView($view);
+    }
+
     public function createAction(Request $request)
     {
         $em = $this->getDoctrine()->getManager();
@@ -30,6 +50,33 @@ class SemaphoreController extends FOSRestController
         } else {
             $data = array('msg' => "Username can't be null.");
             $view = $this->view($data, 400);
+        }
+
+        return $this->handleView($view);
+    }
+
+    public function updateAction(Request $request, $publicId)
+    {
+        $em = $this->getDoctrine()->getManager();
+        $semaphore = $em->getRepository('FestonAperitifBundle:Semaphore')->findOneByPublicId($publicId);
+
+        if (!$semaphore) {
+            $data = array('msg' => "Can't find this semaphore.");
+            $view = $this->view($data, 404);
+
+            return $this->handleView($view);
+        }
+
+        $privateId = $request->request->get('privateId', null);
+        if ($privateId === null || $privateId !== $semaphore->getPrivateId()) {
+            $data = array('msg' => "You don't have the right to update this semaphore.");
+            $view = $this->view($data, 403);
+        } else {
+            $location = $request->request->get('location', 'not provided');
+            $semaphore->setLocation($location);
+            $em->flush();
+            $data = array('msg' => "Semaphore updated.");
+            $view = $this->view($data, 200);
         }
 
         return $this->handleView($view);
